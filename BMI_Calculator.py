18d357e34f23f1cf795b3c5f80b8ddca6d14f6de
MissDesiree <142279292+MissDesiree@users.noreply.github.com>
2024-03-26 17:48:24 -0400

    Add files via upload

---
+++ BMI_Calculator.py
@@ -0,0 +1,11 @@
+# 1st input: enter height in meters e.g: 1.65
+height = input()
+# 2nd input: enter weight in kilograms e.g: 72
+weight = input()
+
+weight_as_int = int("weight")
+height_as_float = float("height")
+bmi = weight_as_int / height_as_float ** 2
+
+bmi_as_int = int("bmi")
+print("bmi_as_int")
